46d9acabb7e49c99cba6513b595d6466155249ce
Tan Min Zhe <minzhetan@gmail.com>
2025-05-28 20:24:37 +0800

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -4,6 +4,7 @@ const noteSchema = new mongoose.Schema({
     title: {type: String, required: true},
     desc: {type: String, required: false},
     email: {type: String, required: true},
+    imagePath: String,
 }, {timestamps: true});
 
 module.exports = mongoose.model('Note', noteSchema);
